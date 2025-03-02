9b453b2f2cc6040a2b1de8d437eb33192d2e23a7
senpai <raohuraira331.rb@gmail.com>
2025-03-02 22:43:42 +0500

    Added side bar

--- html-css-js/side-bar/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".submenu-header").forEach((header) => {
-    header.addEventListener("click", () => {
-      let parent = header.parentElement;
-      parent.classList.toggle("active");
-    });
-  });
-});
